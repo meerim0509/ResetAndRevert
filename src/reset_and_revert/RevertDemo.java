ee7871a2628ebf92f91a1c2ac9a93b4f676f7fa3
meerim0509 <massakunova@gmail.com>
2026-01-19 21:46:30 +0600

    Demo2

--- src/reset_and_revert/RevertDemo.java
+++ src/reset_and_revert/RevertDemo.java
@@ -3,5 +3,6 @@ package reset_and_revert;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("Demo1");
+        System.out.println("Demo2");
     }
 }
