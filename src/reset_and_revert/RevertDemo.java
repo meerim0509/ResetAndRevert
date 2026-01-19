d47054955c8011d90f935025ffb900cf76cbbe7d
meerim0509 <massakunova@gmail.com>
2026-01-19 22:00:20 +0600

    Revert "My name"
    This reverts commit e4b2b0a1aedde5570272f252ae4fe12e3ed25867.

--- src/reset_and_revert/RevertDemo.java
+++ src/reset_and_revert/RevertDemo.java
@@ -4,6 +4,5 @@ public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("Demo1");
         System.out.println("Demo2");
-        System.out.println(My name is Ali);
     }
 }
